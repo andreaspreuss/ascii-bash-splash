28ec99f95467191971b7c5b8e78a7bd177119cec
Andreas Preuss <andreaspreuss@users.noreply.github.com>
2018-05-25 12:25:59 +0200

    Update ascii.py

--- ascii.py
+++ ascii.py
@@ -1,5 +1,5 @@
 import random
 import os
-i = random.randrange(9) + 1 
-website="https://raw.githubusercontent.com/DanCRichards/ASCII-Art-Splash-Screen/master/art/"+ str(i) + ".txt"
+i = random.randrange(19) + 1 
+website="https://raw.githubusercontent.com/andreaspreuss/ascii-bash-splash/art/"+ str(i) + ".txt"
 os.system("curl " +  website)
